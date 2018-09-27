bc5f62ae95a26d75fb8410ca269f00e6918f5925
Sybidlo Tomasz <sybidlo.tomasz26@gmail.com>
2018-09-27 09:27:56 +0200

    -added timer test stub

---
+++ engine/core/test/TimerTest.cpp
@@ -0,0 +1,24 @@
+#define BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE
+#include <boost/test/unit_test.hpp>
+
+#include <OSE/TypeDefs.h>
+#include <OSE/System/Timer.h>
+
+//
+//OSE::Timer::clock::time_point Now()
+//{
+//    std::chrono::milliseconds duration{ ms };
+//    OSE::Timer::clock::time_point now{ duration };
+//
+//    return now;
+//}
+
+BOOST_AUTO_TEST_CASE(TimerEmpty)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(TimerReset)
+{
+
+}
